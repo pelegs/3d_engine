8952b870e41c019abc26364d6e5e21503fa91fff
pelegs <pelegs@gmail.com>
2019-01-03 21:36:16 +0100

    It works!

--- lib/lib3d.py
+++ lib/lib3d.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pygame
 from pygame_colors import WHITE
 
 
@@ -24,15 +25,18 @@ def rotation_matrix(a=0, b=0, c=0):
     ca, sa = np.cos(a), np.sin(a)
     cb, sb = np.cos(b), np.sin(b)
     cc, sc = np.cos(c), np.sin(c)
-    rot_x = np.array([[1, 0, 0],
-                      [0, ca, -sa],
-                      [0, sa, ca]])
-    rot_y = np.array([[cb, 0, sb],
-                      [0, 1, 0],
-                      [-sb, 0, cb]])
-    rot_z = np.array([[cc, -sc, 0],
-                      [sc, cc, 0],
-                      [0, 0, 1]])
+    rot_x = np.array([[1, 0, 0, 0],
+                      [0, ca, -sa, 0],
+                      [0, sa, ca, 0],
+                      [0, 0, 0, 1]])
+    rot_y = np.array([[cb, 0, sb, 0],
+                      [0, 1, 0, 0],
+                      [-sb, 0, cb, 0],
+                      [0, 0, 0, 1]])
+    rot_z = np.array([[cc, -sc, 0, 0],
+                      [sc, cc, 0, 0],
+                      [0, 0, 1, 0],
+                      [0, 0, 0, 1]])
     return np.dot(rot_z, np.dot(rot_y, rot_x))
 
 
@@ -46,16 +50,18 @@ class coordinate_system:
                  pos=[0, 0, 0],
                  a=0, b=0, c=0):
         # Rotation block
-        rotation_block = rotation_matrix(a, b, c)
-        zeros = np.zeros(shape=(3,4))
-        self.transformation_matrix = zeros
-        self.transformation_matrix[:,:-1] = rotation_block
-        # Translation row
-        position = vector(pos, 1)
-        self.transformation_matrix = np.vstack((self.transformation_matrix, position))
+        self.transformation_matrix = rotation_matrix(a, b, c)
+        self.transformation_matrix[-1] = vector(pos, 1)
         # Inverse matrix
         self.transformation_matrix_inv = np.linalg.inv(self.transformation_matrix)
 
+    def translate(self, vec):
+        self.transformation_matrix[-1] += vec
+        self.transformation_matrix_inv = np.linalg.inv(self.transformation_matrix)
+
+    def rotate(self, a=0, b=0, c=0):
+        self.transformation_matrix = np.dot(rotation_matrix(a, b, c), self.transformation_matrix)
+        self.transformation_matrix_inv = np.linalg.inv(self.transformation_matrix)
 
 class object:
     """
@@ -69,6 +75,19 @@ class object:
         self.coord_sys = coord_sys
         self.pos = vector(pos, 1)
         self.color = color
+        self.neighbors = []
+
+    def add_neighbor(self, neighbor):
+        self.neighbors.append(neighbor)
+        if self not in neighbor.neighbors:
+            neighbor.neighbors.append(self)
+
+    def add_neighbors(self, list):
+        for neighbor in list:
+            self.add_neighbor(neighbor)
+
+    def reset_neighbors(self):
+        self.neighbors = []
 
 
 class camera:
@@ -78,9 +97,11 @@ class camera:
     def __init__(self,
                  pos=[0, 0, 0],
                  a=0, b=0, c=0,
+                 screen=None,
                  sW=640, sH=480):
         self.coord_sys = coordinate_system(pos=pos,
                                            a=a, b=b, c=c)
+        self.screen = screen
         self.sW = sW
         self.sH = sH
         self.objects = []
@@ -91,17 +112,35 @@ class camera:
     def add_object(self, obj):
         self.objects.append(obj)
 
-    def draw_object(self, screen):
+    def draw_object(self, obj):
+        # NOTE: Should first transform using own coords,
+        # at the moment only world coordinate system exists
+        pos_cam = np.dot(obj.pos, self.coord_sys.transformation_matrix)
+        if pos_cam[2] != 0:
+            # Draw points
+            screen_x = int(-1 * (pos_cam[0] / pos_cam[2]) * self.sW) + self.sW // 2
+            screen_y = int(-1 * (pos_cam[1] / pos_cam[2]) * self.sH) + self.sH // 2
+            pos_screen = (screen_x, screen_y)
+            pygame.draw.circle(self.screen, obj.color, pos_screen, 3)
+
+            # Draw lines
+            for neighbor in obj.neighbors:
+                pos_cam2 = np.dot(neighbor.pos, self.coord_sys.transformation_matrix)
+                screen_x2 = int(-1 * (pos_cam2[0] / pos_cam2[2]) * self.sW) + self.sW // 2
+                screen_y2 = int(-1 * (pos_cam2[1] / pos_cam2[2]) * self.sH) + self.sH // 2
+                pos_screen2 = (screen_x2, screen_y2)
+                pygame.draw.line(self.screen, obj.color, pos_screen, pos_screen2, 2)
+
+    def draw_all_objects(self):
         for obj in self.objects:
-            # NOTE: Should first transform using own coords,
-            # at the moment only world coordinate system exists
-            pos_cam = np.dot(obj.pos, self.coord_sys.transformation_matrix)
-            if pos_cam[2] != 0:
-                screen_x = int(-1 * (pos_cam[0] / pos_cam[2]) * self.sW) + self.sW // 2
-                screen_y = int(-1 * (pos_cam[1] / pos_cam[2]) * self.sH) + self.sH // 2
-                pos_screen = (screen_x, screen_y)
-                print(pos_screen)
+            self.draw_object(obj)
+
+    def translate(self, vec):
+        self.coord_sys.translate(vec)
+
+    def rotate(self, a=0, b=0, c=0):
+        self.coord_sys.rotate(a, b, c)
 
 
-##### Tests? #####
-
+def make_neighbors(obj1, obj2):
+    obj1.add_neighbor(obj2)
--- test1.py
+++ test1.py
@@ -4,38 +4,91 @@ import pygame
 import sys
 sys.path.append('lib')
 from lib3d import *
+from pygame_colors import BLACK, WHITE
 
 
-WIDTH = 640
-HEIGHT = 480
+WIDTH = 600
+HEIGHT = 600
 FPS = 30
 
-# Inititialize 3d stuff
-world_coord = coordinate_system()
-cam = camera([0, 0, 5])
-points = [object(world_coord, [i, j, k])
-          for i in range(2)
-          for j in range(2)
-          for k in range(2)]
 
-for i, p in enumerate(points):
-    cam.add_object(p)
+def move(x, y, z):
+    dir = vector([x, y, z])
+    cam.translate(dir)
 
-cam.draw_object(screen=1)
-sys.exit()
+def handle_keys():
+    key = pygame.key.get_pressed()
+    if key[pygame.K_UP]:
+        move(0, 0, -0.01)
+    if key[pygame.K_DOWN]:
+        move(0, 0, 0.01)
+    if key[pygame.K_a]:
+        cam.rotate(0, 0, np.pi/500)
+    if key[pygame.K_d]:
+        cam.rotate(0, 0, -np.pi/500)
+    if key[pygame.K_LEFT]:
+        cam.rotate(0, np.pi/500, 0)
+    if key[pygame.K_RIGHT]:
+        cam.rotate(0, -np.pi/500, 0)
+    if key[pygame.K_w]:
+        cam.rotate(np.pi/500, 0, 0)
+    if key[pygame.K_x]:
+        cam.rotate(-np.pi/500, 0, 0)
+
+
 # Inititialize pygame and create a window
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Title')
-clock = pygame.time.clock()
+clock = pygame.time.Clock()
+
+# Inititialize 3d stuff
+world_coord = coordinate_system()
+cam = camera([0, 0, 5], screen=screen, sW=WIDTH, sH=HEIGHT)
+points = [object(world_coord, [i, j, k])
+          for i in [-1, 1]
+          for j in [-1, 1]
+          for k in [-1, 1]]
+make_neighbors(points[0], points[1])
+make_neighbors(points[0], points[2])
+make_neighbors(points[0], points[4])
+make_neighbors(points[1], points[3])
+make_neighbors(points[2], points[6])
+make_neighbors(points[2], points[3])
+make_neighbors(points[3], points[7])
+make_neighbors(points[4], points[5])
+make_neighbors(points[4], points[6])
+make_neighbors(points[5], points[1])
+make_neighbors(points[5], points[7])
+make_neighbors(points[6], points[7])
+
+# TEMP
+trans_vec = vector([0, 0, 0])
 
 # Game loop
 running = True
 while running:
     # Inputs
-    # Screen update
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_q:
+                running = False
+                break
+    handle_keys()
+
+    # Actions
+    for i, p in enumerate(points):
+        cam.add_object(p)
+
     # Draw / Render
-    screen.fill(color)
+    screen.fill(BLACK)
+    cam.draw_all_objects()
+
+    # Reset
+    cam.reset_objects()
 
     # Flip display (after drawing everything)
     pygame.display.flip()
+
+pygame.quit()
+sys.exit()
